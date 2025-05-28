addc69fe5882a2c3af6732fd02dbdf54aa4a9c85
Ranjith Kumar <ranjithrk18121@gmail.com>
2025-05-28 14:11:22 +0530

    add

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Link } from "react-router";
 
-function home() {
+function Home() {
   return (
-    <div>home</div>
-  )
+    <>
+      <section className="flex flex-col items-center justify-center min-h-[80vh] px-4">
+        <div className="text-gray-800 text-3xl font-bold mb-4">
+          Welcome to Our Event Registration Page
+        </div>
+        <button className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-lg shadow-lg text-lg transition duration-300">
+          <Link to={'/login'}>
+          Enter Now To Register
+          </Link>
+        </button>
+      </section>
+    </>
+  );
 }
-
-export default home
+export default Home;
