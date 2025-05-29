e74eef99f485b2f6175d6614387007d48c13b573
Ranjith Kumar <ranjithrk18121@gmail.com>
2025-05-29 10:36:08 +0530

    approutes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "./pages/Home";
 import About from "../pages/About";
 import DevelopersPage from "../pages/DevelopersPage";
 import NotFound from "../pages/NotFound";
+import BasicLayouts from "../layouts/BasicLayouts";
 
 const AppRoutes = () => {
     return (
         <Routes>
+            <Route path="/" element={<BasicLayouts />}>
             <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/developers" element={<DevelopersPage />} />
             <Route path="*" element={<NotFound />} />
+            </Route>
         </Routes>
     );
 }
