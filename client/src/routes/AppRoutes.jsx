470a4d83c39f5bfa6c885742fab72988308bc4bd
Bala-K3 <balakgbhb@gmaik.com>
2025-05-30 20:02:09 +0530

    8th Commit

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -2,15 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import BasicLayout from '../layouts/BasicLayouts';
 import DashboardLayout from '../layouts/DashBoardLayout';
 import PrivateRoutes from './PrivatesRoutes';
-<<<<<<< HEAD
-import Home from '../pages/Home';
-import ServicePage from '../pages/ServicePage';
-import GalleryPage from '../pages/GalleryPage';
-import TestimonialPage from '../pages/TestimonalPage';
-import ContactPage from '../pages/ContactPage';
-=======
 import LandingPage from '../pages/LandingPage';
->>>>>>> 5dc93140126f30822e9c5e3f6fe4161783234c98
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import NotFound from '../pages/NotFound';
@@ -21,16 +13,7 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<BasicLayout />}>
-<<<<<<< HEAD
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<ServicePage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-        <Route path="/testimonials" element={<TestimonialPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-=======
         <Route path="/" element={<LandingPage />} />
->>>>>>> 5dc93140126f30822e9c5e3f6fe4161783234c98
         <Route path="/login" element={<SignIn />} />
       <Route path="/register" element={<SignUp />} />
       </Route>
